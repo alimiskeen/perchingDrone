6326bce0c65a271fabdebd7d97635f99e6a2bc13
Ali A. Almiskeen <alimiskeen@me.com>
2021-03-13 11:24:17 -0700

    wrote test controller.py

--- src/controlGUI/controller.py
+++ src/controlGUI/controller.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+
+import rospy
+from perchingDrone.msg import core_command, hook_command, hook_status
+
+
+if __name__ == '__main__':
+    rospy.init_node("controller")
+
+    status_topic = rospy.Publisher('status_topic', hook_status, queue_size=5)
+
+    rate = rospy.Rate(1)
+
+    msg = hook_status()
+    msg.hook_open = True
+    msg.core_open = False
+
+    while not rospy.is_shutdown():
+        msg.hook_open = not msg.hook_open
+        msg.core_open = not msg.core_open
+
+        status_topic.publish(msg)
+
+        rate.sleep()
