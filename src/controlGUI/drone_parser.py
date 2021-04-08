f695ee7fa9a60540473e4d4c6e0e9b42ca66ae17
Ali A. Almiskeen (ubuntu) <alimiskeen@me.com>
2021-04-08 03:32:58 -0600

    setup the drone to take off and land test

--- src/controlGUI/drone_parser.py
+++ src/controlGUI/drone_parser.py
@@ -45,30 +45,89 @@ def initialize_drone(connection_string: str):
     return vehicle
 
 
+def empty_print(text: str):
+    pass
+
+
+def arm(vhcl, print_method=empty_print):
+    print_method('arming the drone')
+    vhcl.armed = True
+    while not vhcl.armed:
+        time.sleep(.1)
+        print_method('drone not armed yet')
+    print_method('drone armed')
+
+
+def disarm(vhcl, print_method=empty_print):
+    print_method('disarming the drone')
+    vhcl.armed = False
+    while vhcl.armed:
+        time.sleep(.1)
+        print_method('drone not disarmed yet')
+    print_method("disarmed")
+
+
+# Function to arm and then takeoff to a user specified altitude
+def arm_and_takeoff(vhcl, aTargetAltitude):
+    print("Basic pre-arm checks")
+    # Don't let the user try to arm until autopilot is ready
+    while not vhcl.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vhcl.mode = VehicleMode("GUIDED")
+    vhcl.armed = True
+
+    while not vhcl.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vhcl.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+
+    # Check that vehicle has reached takeoff altitude
+    while True:
+        print(" Altitude: ", vhcl.location.global_relative_frame.alt)
+        # Break and return from function just below target altitude.
+        if vhcl.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+
+def land(vhcl):
+    pass
+
+
+
 conn_str = '/dev/ttyACM0'
 vehicle = initialize_drone(conn_str)
 
 print('change the drone mode to loiter')
 change_mode("LOITER")
 
-print('arming the drone')
-vehicle.armed = True
+arm(vehicle)
 
-while not vehicle.armed:
-    time.sleep(1)
-    print('drone not armed yet')
-
-print('drone armed')
 print(f'state: {vehicle.system_status.state}')
-
 time.sleep(5)
 
-print('disarming the drone')
-vehicle.armed = False
+arm_and_takeoff(vehicle, 2)
 
-while vehicle.armed:
-    time.sleep(.5)
-    print('drone not disarmed yet')
+print('reached altitude')
+
+time.sleep(10)
+
+print('now going to land')
+
+vehicle.mode = VehicleMode("LAND")
+
+print('now we wait till we reach the ground')
+time.sleep(20)
+
+print('disarming the drone')
+disarm(vehicle)
 
 # print(vehicle.system_status.state) # good thing to get on the gui
 print(vehicle.system_status.state)
