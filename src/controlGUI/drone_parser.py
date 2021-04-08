62fbb54e3ed99b9bfc221cedd71b7f21a9b68c18
Ali A. Almiskeen (ubuntu) <alimiskeen@me.com>
2021-04-08 02:57:18 -0600

    now can connect and arm the pixhawk

--- src/controlGUI/drone_parser.py
+++ src/controlGUI/drone_parser.py
@@ -16,18 +16,61 @@ def change_mode(mode: str):
     # print(f'mode changed to {vehicle.mode}')
 
 
-connection_string = '/dev/ttyACM0'
+def initialize_drone(connection_string: str):
+    """
+    a method that initializes a drone vehicle and returns a vehicle
+    that is ready to arm
+    :param connection_string: str
+    :return: dronekit vehicle
+    """
+    try:
+        print(f"Connecting to vehicle on: {connection_string}")
+        vehicle = connect(connection_string, wait_ready=True)
+    except:
+        print("running the shell script")
+        os.system('sudo ./usbpermission.sh')
+        time.sleep(3)
+        vehicle = connect(connection_string, wait_ready=True)
 
-try:
-    print("Connecting to vehicle on: %s" % (connection_string,))
-    vehicle = connect(connection_string, wait_ready=True)
-except:
-    print("need to run shell script first")
-    os.system('sudo ./usbpermission.sh')
-    time.sleep(3)
-    vehicle = connect(connection_string, wait_ready=True)
+    print('reading GPS signal')
+    while vehicle.gps_0.satellites_visible < 5:
+        print(f'not enough GPS sats: {vehicle.gps_0.satellites_visible}')
+        time.sleep(1)
+
+    print('waiting for the drone to become armable')
+    while not vehicle.is_armable:
+        time.sleep(1)
+        print('drone not yet armable')
+
+    return vehicle
 
 
-change_mode("GUIDED")
+conn_str = '/dev/ttyACM0'
+vehicle = initialize_drone(conn_str)
+
+print('change the drone mode to loiter')
+change_mode("LOITER")
+
+print('arming the drone')
+vehicle.armed = True
+
+while not vehicle.armed:
+    time.sleep(1)
+    print('drone not armed yet')
+
+print('drone armed')
+print(f'state: {vehicle.system_status.state}')
+
+time.sleep(5)
+
+print('disarming the drone')
+vehicle.armed = False
+
+while vehicle.armed:
+    time.sleep(.5)
+    print('drone not disarmed yet')
+
+# print(vehicle.system_status.state) # good thing to get on the gui
+print(vehicle.system_status.state)
 
 vehicle.close()
