a6f787f0207404384ccfa7289d33d32c67e3725a
Ali A. Almiskeen <alimiskeen@me.com>
2021-04-14 17:26:08 -0600

    added yaw

--- src/controlGUI/key.py
+++ src/controlGUI/key.py
@@ -97,6 +97,30 @@ def open_hook():
     print('open hook')  # remove this later
 
 
+def condition_yaw(heading, relative=False):
+    if relative:
+        is_relative = 1  # yaw relative to direction of travel
+    else:
+        is_relative = 0  # yaw is an absolute angle
+    # create the CONDITION_YAW command using command_long_encode()
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,  # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW,  # command
+        0,  # confirmation
+        heading,  # param 1, yaw in degrees
+        0,  # param 2, yaw speed deg/s
+        1,  # param 3, direction -1 ccw, 1 cw
+        is_relative,  # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)  # param 5 ~ 7 not used
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+
+
+def yaw(angle):
+    if angle != 0:
+        condition_yaw(angle, relative=True)
+
+
 # -- Key event function
 def key(event):
     if event.char == event.keysym:  # -- standard keys
@@ -127,6 +151,10 @@ def key(event):
             close_hook()
         elif event.keysym == 'h':
             open_hook()
+        elif event.keysym == 'q':
+            yaw(-5)
+        elif event.keysym == 'e':
+            yaw(5)
 
 # ---- MAIN FUNCTION
 # - Takeoff
