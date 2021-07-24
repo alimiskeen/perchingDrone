4f0aca97755b44a47a713a488c377a6c5d0dd86c
Ali A. Almiskeen <alimiskeen@me.com>
2021-07-24 02:46:08 -0600

    prep for dronekit in ps4controller.py

--- src/controlGUI/ps4controller.py
+++ src/controlGUI/ps4controller.py
@@ -1,11 +1,12 @@
-from pyPS4Controller.controller import Controller
-# requires pip3/pip install pyPS4Controller
+from pyPS4Controller.controller import Controller  # requires pip3/pip install pyPS4Controller
+# from dronekit import connect, VehicleMode, LocationGlobal
 
 
 class DroneController(Controller):
 
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
+        # init connection to the drone
 
     def on_x_press(self):
         """ Let the drone take off """
@@ -32,6 +33,7 @@ class DroneController(Controller):
         pass
 
     """ All the methods to control the right joystick """
+
     def on_R3_down(self, value):
         pass
 
@@ -45,6 +47,7 @@ class DroneController(Controller):
         pass
 
     """ All the methods to control the left joystick """
+
     def on_L3_down(self, value):
         pass
 
@@ -58,8 +61,6 @@ class DroneController(Controller):
         pass
 
 
-
 if __name__ == '__main__':
-
-    ctrl = DroneController(interface="/dev/input/js0", connecting_using_ds4drv=False, )
+    ctrl = DroneController(interface="/dev/input/js0", connecting_using_ds4drv=False)
     ctrl.listen(timeout=60)
