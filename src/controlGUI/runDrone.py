4a2319f7422299d3145af2b11eb9c3b6e587bd05
Ali A. Almiskeen <42902454+alimiskeen@users.noreply.github.com>
2021-08-30 09:14:28 -0600

    Update runDrone.py
    testing the drone commands

--- src/controlGUI/runDrone.py
+++ src/controlGUI/runDrone.py
@@ -18,10 +18,35 @@ if __name__ == '__main__':
     drn = Drone("/dev/ttyACM0")
     print('connected to the drone')
 
-    print('connecting to controller')
-    controller = DroneController(drn, interface="/dev/input/js0", connecting_using_ds4drv=False)
-    print('connected, now listening')
-    controller.listen(on_connect=controller_connected, on_disconnect=controller_disconnected)
+    # print('connecting to controller')
+    # controller = DroneController(drn, interface="/dev/input/js0", connecting_using_ds4drv=False)
+    # print('connected, now listening')
+    # controller.listen(on_connect=controller_connected, on_disconnect=controller_disconnected)
+
+    print('arming')
+    drn.arm_drone()
+
+    print('taking off')
+    drn.takeoff(1.0)
+
+    time.sleep(5)
+
+    print('moving up')
+    drn.move(0.0, 0.0, 0.2)
+
+    time.sleep(5)
+
+    print('stopping and landing')
+    drn.move(0.0, 0.0, 0.0)
+
+    drn.land()
+
+    time.sleep(10)
+
+    print('disarming')
+    drn.disarm_drone()
+
+
 
     # controller = Controller(interface="/dev/input/js0", connecting_using_ds4drv=False)
     # controller.listen()
