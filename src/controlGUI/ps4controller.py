a2d101de95b28447dbd56a02385ed9902216f00f
Ali A. Almiskeen <alimiskeen@me.com>
2021-08-31 19:26:49 -0600

    reimagined controller

--- src/controlGUI/ps4controller.py
+++ src/controlGUI/ps4controller.py
@@ -1,3 +1,5 @@
+import time
+
 from pyPS4Controller.controller import Controller  # requires pip3/pip install pyPS4Controller
 from drone import Drone
 
@@ -35,71 +37,61 @@ class DroneController(Controller):
 
     def on_R1_press(self):
         """ Open and Close the hook """
-        if self.is_hook_open:
-            #close the hook
-            self.is_hook_open = False
-        else:
-            # open hook
-            self.is_hook_open = True
+        # if self.is_hook_open:
+        #     #close the hook
+        #     self.is_hook_open = False
+        # else:
+        #     # open hook
+        #     self.is_hook_open = True
+        self.rise = 0.5
+        self._write_joystick_data()
 
     def on_L1_press(self):
         """ Open and Close the coil """
-        if self.is_core_open:
-            # close core
-            self.is_core_open = False
-        else:
-            # open core
-            self.is_core_open = True
+        # if self.is_core_open:
+        #     # close core
+        #     self.is_core_open = False
+        # else:
+        #     # open core
+        #     self.is_core_open = True
+        self.rise = -0.5
+        self._write_joystick_data()
 
 
     """ All the methods to control the right joystick """
 
-    def on_R3_down(self, value):
-        self.forward = value / 33000.0
+    def on_left_arrow_press(self):
+        self.sideways = -0.5
         self._write_joystick_data()
 
-    def on_R3_up(self, value):
-        self.forward = value / 33000.0
+    def on_right_arrow_press(self):
+        self.sideways = 0.5
         self._write_joystick_data()
 
-    def on_R3_left(self, value):
-        self.sideways = value / 33000.0
+    def on_up_arrow_press(self):
+        self.forward = 0.5
         self._write_joystick_data()
 
-    def on_R3_right(self, value):
-        self.sideways = value / 33000.0
+    def on_down_arrow_press(self):
+        self.forward = -0.5
         self._write_joystick_data()
 
-    def on_R3_x_at_rest(self):
+    def on_left_right_arrow_release(self):
         self.sideways = 0.0
         self._write_joystick_data()
 
-    def on_R3_y_at_rest(self):
+    def on_up_down_arrow_release(self):
         self.forward = 0.0
         self._write_joystick_data()
 
     """ All the methods to control the left joystick """
 
-    def on_L3_down(self, value):
-        self.rise = value / 33000.0
-        self._write_joystick_data()
+    def on_options_press(self):
+        self.drone.yaw(5)
 
-    def on_L3_up(self, value):
-        self.rise = value / 33000.0
-        self._write_joystick_data()
+    def on_share_press(self):
+        self.drone.yaw(-5)
 
-    def on_L3_left(self, value):
-        self.drone.yaw(value / 3300.0)
-
-    def on_L3_right(self, value):
-        self.drone.yaw(value / 3300.0)
-
-    def on_L3_x_at_rest(self):
-        self.drone.yaw(0.0)
-
-    def on_L3_y_at_rest(self):
-        self.rise = 0.0
-        self._write_joystick_data()
 
     def _write_joystick_data(self):
         self.drone.move(self.sideways, self.forward, self.rise)
